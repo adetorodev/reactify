ac2a169da6eee998b1cd75948b07845b8b6ac009
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-12-16 00:03:56 +0100

    task: functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import ClassCompt from "./component/ClassCompt";
 import FuncCompt from "./component/FuncCompt";
+import Greet from "./component/Greet";
+
+
+// import MyComponent from "./component/Greet";
+
 
 function App() {
   return <div>
-    <FuncCompt name="Ezekiel" />
-    <ClassCompt name="class Component"/>
+    {/* <FuncCompt name="Ezekiel" />
+    <ClassCompt name="class Component"/> */}
+
+    <Greet />
   </div>;
 }
 
