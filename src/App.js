a280f864c9656bdffc726982c2e827ffac95f9e2
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-12-19 23:06:17 +0100

    task: Ref

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import FragmentDemo from "./component/FragmentDemo";
 import ParentCompt from "./component/ParentCompt";
 import PureCompt from "./component/PureComponent";
+import RefsDemo from "./component/RefsDemo";
 import Table from "./component/Table";
 
 function App() {
   return (
     <div>
+      <RefsDemo />
      {/* <PureCompt /> */}
-     <ParentCompt />
+     {/* <ParentCompt /> */}
   </div>
   )
 }
