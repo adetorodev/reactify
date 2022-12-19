76c9f764f1eb9be83c17d53b241f916e8d971682
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-12-19 22:49:27 +0100

    task: memo

--- src/component/ParentCompt.js
+++ src/component/ParentCompt.js
@@ -1,38 +1,10 @@
 import React, { Component, PureComponent} from 'react'
 import RegularCompt from './RegularCompt'
 import PureCompt from './PureComponent'
+import MemoComponentt from './memoComponentt'
+// import MemoCompt from './MemoCompt'
 
-// class ParentCompt extends Component {
-//     constructor(props) {
-//       super(props)
-    
-//       this.state = {
-//          name: "Ezekiel"
-//       }
-//     }
-//     componentDidMount() {
-//         setTimeout(() => {
-//             this.setState({
-//                 name: "Ezekiel"
-//             })
-//         }, 2000)
-//     }
-//   render() {
-//     console.log("**** parent Component rendered********")
-//     return (
-//       <div>
-//         Pure Compoent
-//         <RegularCompt name={this.state.name} />
-//         <PureCompt name={this.state.name} />
-//       </div>
-//     )
-//   }
-// }
-
-
-//  No re-render
-// dont mutate object or array 
-class ParentCompt extends PureComponent {
+class ParentCompt extends Component {
     constructor(props) {
       super(props)
     
@@ -52,11 +24,43 @@ class ParentCompt extends PureComponent {
     return (
       <div>
         Pure Compoent
-        <RegularCompt name={this.state.name} />
-        <PureCompt name={this.state.name} />
+        <MemoComponentt name={this.state.name}/>
+        {/* <RegularCompt name={this.state.name} /> */}
+        {/* <PureCompt name={this.state.name} /> */}
       </div>
     )
   }
 }
+
+
+//  No re-render
+// dont mutate object or array 
+// class ParentCompt extends PureComponent {
+//     constructor(props) {
+//       super(props)
+    
+//       this.state = {
+//          name: "Ezekiel"
+//       }
+//     }
+//     componentDidMount() {
+//         setTimeout(() => {
+//             this.setState({
+//                 name: "Ezekiel"
+//             })
+//         }, 2000)
+//     }
+//   render() {
+//     console.log("**** parent Component rendered********")
+//     return (
+//       <div>
+//         Pure Compoent
+        
+//         {/* <RegularCompt name={this.state.name} /> */}
+//         {/* <PureCompt name={this.state.name} /> */}
+//       </div>
+//     )
+//   }
+// }
 
 export default ParentCompt
