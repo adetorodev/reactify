7ed4eb87e3f6b47ce96d702b27a7000c52e18e51
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-12-20 21:22:27 +0100

    task: error boundary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Form from "./component/Form";
+import Hero from "./component/Hero";
+import HeroBoundary from "./component/HeroBoundary";
 
 function App() {
-  return <div>
-    <Form />
-  </div>;
+  return (
+    <div>
+      <HeroBoundary>
+        <Hero heroName="Joker" />
+      </HeroBoundary>
+      <HeroBoundary>
+        <Hero heroName="SuperMan" />
+      </HeroBoundary>
+      <HeroBoundary>
+        <Hero heroName="Batman" />
+      </HeroBoundary>
+
+      {/* <Form /> */}
+    </div>
+  );
 }
 
 export default App;
- 
